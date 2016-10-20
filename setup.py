737d75ccbffcd6596fc767905d83c1dffda60514
Kenji Wellman <kenji.wellman@gmail.com>
2016-10-20 10:52:40 -0400

    add url and change email in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='lektor-data-pages',
     version='0.1',
     author=u'Kenji Wellman',
-    author_email='kenji.wellman@gmail.com',
+    author_email='kenji.wellman@yargies.com',
+    url='https://github.com/yargies/lektor-datapages', 
     license='MIT',
     py_modules=['lektor_data_pages'],
     entry_points={
